ad8dc5e3723907db580bb79c7c861356e222ca2f
BurningJump <935646729@qq.com>
2018-01-14 16:06:49 +0800

    完成中国国旗的绘制

--- flags.js
+++ flags.js
@@ -5,17 +5,42 @@ var height = ctx.canvas.height;
 
 //中国
 function drawChina() {
+	
 	ctx.save();
 	ctx.fillStyle="red";
 	ctx.fillRect(0, 0, width, height);
+
+	//大五角星
+	//外接圆半径
+	R = width/10;
+	//内接圆半径
+	r = R * Math.sin(18*2*Math.PI/360)/Math.sin(126*2*Math.PI/360);
+	x = width/6;
+	y = height/4;
+	star(ctx, R, r, x, y, 0);
+
+	//小五角星
+	//外接圆半径
+	sR = width/30;
+	//内接圆半径
+	sr = sR * Math.sin(18*2*Math.PI/360)/Math.sin(126*2*Math.PI/360);
+	star(ctx, sR, sr, width/3, height/10, -Math.atan(5/3)*180/Math.PI);
+	star(ctx, sR, sr, width*2/5, height/5, -Math.atan(7)*180/Math.PI);
+	star(ctx, sR, sr, width*2/5, height*7/20, 0);
+	star(ctx, sR, sr, width/3, height*9/20, -Math.atan(5/4)*180/Math.PI);
 }
-//drawChina();
+drawChina();
 
 //五角星
-function star() {
+function star( ctx, R, r, x, y, rotate) {
 	ctx.save();
 	ctx.beginPath();
-	ctx.arc(width/2, height/2, height/2-5, 0, 2*Math.PI, false);
-	ctx.stroke();
+	for(var i = 0; i < 5; i++) {
+		ctx.lineTo(Math.cos((18+i*72-rotate)/180*Math.PI)*R + x, -Math.sin((18+i*72-rotate)/180*Math.PI)*R + y);
+		ctx.lineTo(Math.cos((54+i*72-rotate)/180*Math.PI)*r + x, -Math.sin((54+i*72-rotate)/180*Math.PI)*r + y);
+	}
+	ctx.closePath();
+	ctx.fillStyle = 'yellow';
+	ctx.fill();
+	ctx.restore();
 }
-star();
