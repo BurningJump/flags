c0cddca2a9bcba48fccf7a82a50562d0a9a5e127
BurningJump <935646729@qq.com>
2018-01-15 22:55:49 +0800

    新增俄罗斯

--- flags.js
+++ flags.js
@@ -3,6 +3,20 @@ var ctx=flag.getContext("2d");
 var width = ctx.canvas.width;
 var height = ctx.canvas.height;
 
+//正五角星
+function star( ctx, R, r, x, y, rotate, color) {
+	ctx.save();
+	ctx.beginPath();
+	for(var i = 0; i < 5; i++) {
+		ctx.lineTo(Math.cos((18+i*72-rotate)/180*Math.PI)*R + x, -Math.sin((18+i*72-rotate)/180*Math.PI)*R + y);
+		ctx.lineTo(Math.cos((54+i*72-rotate)/180*Math.PI)*r + x, -Math.sin((54+i*72-rotate)/180*Math.PI)*r + y);
+	}
+	ctx.closePath();
+	ctx.fillStyle = color;
+	ctx.fill();
+	ctx.restore();
+}
+
 //中国
 function drawChina() {
 	
@@ -28,20 +42,6 @@ function drawChina() {
 	star(ctx, sR, sr, width*2/5, height/5, -Math.atan(7)*180/Math.PI, 'yellow');
 	star(ctx, sR, sr, width*2/5, height*7/20, 0, 'yellow');
 	star(ctx, sR, sr, width/3, height*9/20, -Math.atan(5/4)*180/Math.PI, 'yellow');
-}
-
-//五角星
-function star( ctx, R, r, x, y, rotate, color) {
-	ctx.save();
-	ctx.beginPath();
-	for(var i = 0; i < 5; i++) {
-		ctx.lineTo(Math.cos((18+i*72-rotate)/180*Math.PI)*R + x, -Math.sin((18+i*72-rotate)/180*Math.PI)*R + y);
-		ctx.lineTo(Math.cos((54+i*72-rotate)/180*Math.PI)*r + x, -Math.sin((54+i*72-rotate)/180*Math.PI)*r + y);
-	}
-	ctx.closePath();
-	ctx.fillStyle = color;
-	ctx.fill();
-	ctx.restore();
 }
 //drawChina();
 
@@ -164,4 +164,17 @@ function drawUSA(argument) {
 		}
 	}
 }
-drawUSA();
+//drawUSA();
+
+//俄罗斯
+function drawRussia() {
+	ctx.save();
+	ctx.fillStyle = "#fff";
+	ctx.fillRect(0, 0, width, height/3);
+	ctx.fillStyle = "rgb(0, 81, 186)";
+	ctx.fillRect(0, height/3, width, height/3);
+	ctx.fillStyle = "rgb(216, 30, 5)";
+	ctx.fillRect(0, height/3*2, width, height/3);
+	ctx.restore();
+}
+drawRussia();
