d332db42a1c99e93123a06741dc6e692eb3e40fc
BurningJump <935646729@qq.com>
2018-04-02 22:14:02 +0800

    测试excel表格

---
+++ main.js
@@ -0,0 +1,4 @@
+var country = document.getElementById('country');
+var index = country.selectedIndex;
+var country = country.options[document.getElementById('country').selectedIndex].value;
+console.log(country)
